f90a4a9a063e3a311d498ba032cb804ea8beddb1
Your Name <kristaps006@gmail.com>
2021-03-10 12:07:03 +0100

    1st assigment done

--- src/App.js
+++ src/App.js
@@ -2,15 +2,26 @@ import logo from "./logo.svg";
 import "./App.css";
 import React, { Component } from "react";
 import UserOutput from "./UserOutput/UserOutput";
+import UserInput from "./UserInput/UserInput";
 
 class App extends React.Component {
   state = {
-    firstName: ["Mikkel", "Jan", "Theodor"],
+    firstName: [{ name: "Mikkel" }, { name: "Jan" }, { name: "Theodor" }],
   };
 
   nameChange = (newName) => {
     this.setState({
-      firstName: ["Klaus", newName, "John"],
+      firstName: [{ name: "Mikkel" }, { name: "Jan" }, { name: "Theodor" }],
+    });
+  };
+
+  nameInput = (e) => {
+    this.setState({
+      firstName: [
+        { name: e.target.value },
+        { name: "Jan" },
+        { name: e.target.value },
+      ],
     });
   };
 
@@ -33,15 +44,16 @@ class App extends React.Component {
 
         <UserOutput
           buttonclick={this.nameChange.bind(this, "Claus")}
-          username={this.state.firstName[1]}
+          username={this.state.firstName[0].name}
           time="third"
           language="Vue"
         />
         <UserOutput
-          username={this.state.firstName[2]}
+          username={this.state.firstName[2].name}
           time="second"
           language="html"
         />
+        <UserInput change={this.nameInput} />
         <UserOutput username="Marianne" time="fifth" language="css">
           <div>
             This is very hard to write in React when you are not use to it
